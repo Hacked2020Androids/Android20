47ddf602c8dd93ca79ad61f0e078b0b78cb05ff3
Proxieds <victor1@ualberta.ca>
2020-01-18 21:59:54 -0800

    Fixed Signup

--- app/src/main/java/com/example/myapplication/Register.java
+++ app/src/main/java/com/example/myapplication/Register.java
@@ -1,38 +1,116 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.AppCompatButton;
+import androidx.appcompat.widget.AppCompatEditText;
 import androidx.appcompat.widget.AppCompatTextView;
 
 import android.content.Intent;
 import android.graphics.Color;
 import android.os.Build;
 import android.os.Bundle;
+import android.text.Editable;
 import android.text.SpannableString;
 import android.text.Spanned;
 import android.text.style.ForegroundColorSpan;
+import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
 
 public class Register extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        final FirebaseFirestore db;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
             getWindow().setFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS,
                     WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         }
+        // instance of database
+        db = FirebaseFirestore.getInstance();
+
+        final CollectionReference collectionReference = db.collection("User");
+        final AppCompatEditText usernameInfo = findViewById(R.id.username);
+        final Editable usernameParsed = usernameInfo.getText();
+        final AppCompatEditText passwordInfo = findViewById(R.id.password);
+        final Editable pwParsed = passwordInfo.getText();
+        final EditText confirmPW = findViewById(R.id.password_veri);
+        final Editable pwVeriParsed = confirmPW.getText();
+
+        final AppCompatButton registerBtn = findViewById(R.id.signup_btn);
+        registerBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                User newUser = new User(usernameParsed.toString(), pwParsed.toString());
+                if (pwParsed.toString().isEmpty() || usernameParsed.toString().isEmpty() || pwVeriParsed.toString().isEmpty()) {
+                    Toast.makeText(Register.this, "Error: Please add information", Toast.LENGTH_SHORT).show();
+                } else if (!(pwParsed.toString().equals(pwVeriParsed.toString()))) {
+                    Toast.makeText(Register.this, "Passwords Do not Match", Toast.LENGTH_SHORT).show();
+                }
+
+//                Query query = db.collection("users").whereEqualTo("username", newUser.getUsername());
+//                query.get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+//                        if (task.getResult() != null && task.getResult().size() > 0) {
+//                            Toast.makeText(Register.this, "Error: Username already exists", Toast.LENGTH_SHORT).show();
+//                        }
+//                    }
+//                });
+                else {
+                    HashMap<String, Object> user = new HashMap<>();
+                    user.put("username", newUser.getUsername());
+                    user.put("password", newUser.getPassword());
+
+                    final CollectionReference collectionReference = db.collection("User");
+                    collectionReference.document(newUser.getUsername())
+                            .set(user)
+                            .addOnSuccessListener(new OnSuccessListener<Void>() {
+                                @Override
+                                public void onSuccess(Void aVoid) {
+                                    Log.d("thisActivity", "DocumentSnapshot successfully written!");
+                                }
+                            })
+                            .addOnFailureListener(new OnFailureListener() {
+                                @Override
+                                public void onFailure(@NonNull Exception e) {
+                                    Log.w("Failed to add user", "Error writing document", e);
+                                }
+                            });
+                    Toast.makeText(Register.this, "Successfully Signed Up", Toast.LENGTH_SHORT).show();
+                    Intent userInputIntent = new Intent(Register.this, UserInputActivity.class);
+                    startActivity(userInputIntent);
+                    finish();
+                }
+            }
+        });
 
         final AppCompatTextView signUpLink = findViewById(R.id.login_link);
         signUpLink.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent loginScreen = new Intent(Register.this, Login.class);
-                startActivity(loginScreen);
+            public void onClick (View view){
+                Intent signUpScreen = new Intent(Register.this, Login.class);
+                startActivity(signUpScreen);
                 finish();
             }
         });
+
     }
 }
